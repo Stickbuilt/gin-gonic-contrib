38b32b532ad113ae0b8075358df32c69246d08dd
Ben Nichols <ben.nichols@gmail.com>
2016-06-28 15:10:17 -0700

    Fixed ordering of handlers

--- rest/rest.go
+++ rest/rest.go
@@ -29,23 +29,23 @@ type DeleteSupported interface {
 //   POST: /path/:id
 func CRUD(group *gin.RouterGroup, path string, resource interface{}, handlers ...gin.HandlerFunc) {
 	if resource, ok := resource.(CreateSupported); ok {
-		res := append([]gin.HandlerFunc{resource.CreateHandler}, handlers...)
+		res := append(handlers, resource.CreateHandler)
 		group.POST(path, res...)
 	}
 	if resource, ok := resource.(ListSupported); ok {
-		res := append([]gin.HandlerFunc{resource.ListHandler}, handlers...)
+		res := append(handlers, resource.ListHandler)
 		group.GET(path, res...)
 	}
 	if resource, ok := resource.(TakeSupported); ok {
-		res := append([]gin.HandlerFunc{resource.TakeHandler}, handlers...)
+		res := append(handlers, resource.TakeHandler)
 		group.GET(path+"/:id", res...)
 	}
 	if resource, ok := resource.(UpdateSupported); ok {
-		res := append([]gin.HandlerFunc{resource.UpdateHandler}, handlers...)
+		res := append(handlers, resource.UpdateHandler)
 		group.PUT(path+"/:id", res...)
 	}
 	if resource, ok := resource.(DeleteSupported); ok {
-		res := append([]gin.HandlerFunc{resource.DeleteHandler}, handlers...)
+		res := append(handlers, resource.DeleteHandler)
 		group.DELETE(path+"/:id", res...)
 	}
 }
